59c81ac0609598cb2a2e97bd1c713677f170ac03
Bahar TİMAÇ <bahar.timac@gmail.com>
2016-01-15 18:10:33 +0200

    Kullanıcı Değişim ekranı tamamlandı

--- YONETIM/kullanici_degistir_batch.cpp
+++ YONETIM/kullanici_degistir_batch.cpp
@@ -42,13 +42,13 @@ KULLANICI_DEGISTIR_BATCH::KULLANICI_DEGISTIR_BATCH (int *kullanici_id, QWidget *
 
 void KULLANICI_DEGISTIR_BATCH::SETUP_FORM ()
 {
-    SET_NAME_OF_RUN_BATCH_BUTTON ( tr("Kullanıcı Değiştir")      );
+    SET_NAME_OF_RUN_BATCH_BUTTON ( tr("Changing User")      );
     SET_FIRST_FOCUS_WIDGET       ( m_ui->lineEdit_kullanici_kodu );
     REGISTER_BUTTON_WIDGET       ( m_ui->widget_batch_buttons    );
     SET_ENTER_KEY_FOR_RUN_BATCH  ( true  );
     SET_AUTO_EXIT_BATCH ( true );
 
-    SET_PAGE_TITLE    ( tr ( "KULLANICI DEĞİŞTİRME EKRANI" ) ) ;
+    SET_PAGE_TITLE    ( tr ( "CHANGING USER SCREEN" ) ) ;
     SET_SETTING_NAME  ( "KULLANICI_DEGISTIRME" );
     SET_HELP_PAGE     ( "yonetim-islemleri_kullanici-degistir" );
 
@@ -80,7 +80,7 @@ int KULLANICI_DEGISTIR_BATCH::CHECK_VAR ( QObject * p_object )
         sql_query.SET_VALUE      ( ":kullanici_kodu", m_ui->lineEdit_kullanici_kodu->text() );
 
         if ( sql_query.SELECT() EQ 0 ) {
-            MSG_ERROR(tr ( "Kullanıcı bulunamadı" ) , m_ui->lineEdit_kullanici_kodu );
+            MSG_ERROR(tr ( "User not found." ) , m_ui->lineEdit_kullanici_kodu );
             return ADAK_FAIL_UNDO;
         }
         sql_query.NEXT();
@@ -96,7 +96,7 @@ int KULLANICI_DEGISTIR_BATCH::CHECK_VAR ( QObject * p_object )
             sql_query.SET_VALUE      ( ":kullanici_id" , m_kullanici_id  );
 
             if ( sql_query.SELECT() EQ 0 ) {
-                MSG_ERROR( QObject::tr("Kullanıcı bilgisayarda kayıtlı değil.") , m_ui->lineEdit_kullanici_kodu );
+                MSG_ERROR( QObject::tr("The user is not registered on the computer.") , m_ui->lineEdit_kullanici_kodu );//Kullanıcı bilgisayarda kayıtlı değil.
                 return ADAK_FAIL_UNDO;
             }
         }
@@ -114,12 +114,12 @@ int KULLANICI_DEGISTIR_BATCH::CHECK_VAR ( QObject * p_object )
 int KULLANICI_DEGISTIR_BATCH::CHECK_RUN()
 {
     if ( m_ui->lineEdit_kullanici_kodu->text().isEmpty() EQ true ) {
-        MSG_ERROR(tr("Kullanıcı kodu boş bırakılamaz" ) , m_ui->lineEdit_kullanici_kodu);
+        MSG_ERROR(tr("User name can not be empty." ) , m_ui->lineEdit_kullanici_kodu);//Kullanıcı kodu boş bırakılamaz.
         return ADAK_FAIL;
     }
 
     if ( m_kullanici_id < 1 ) {
-        MSG_ERROR(tr ( "Geçerli bir kullanıcı seçilmemiş" ) , m_ui->lineEdit_kullanici_kodu );
+        MSG_ERROR(tr ( "Invalid user" ) , m_ui->lineEdit_kullanici_kodu );//Geçersiz kullanıcı
         return ADAK_FAIL;
     }
 
@@ -135,7 +135,7 @@ int KULLANICI_DEGISTIR_BATCH::CHECK_RUN()
 
     if ( sql_query.SELECT() EQ 0 ) {
         m_ui->lineEdit_kullanici_parola->clear();
-        MSG_ERROR(tr ( "Hatalı parola" ) , m_ui->lineEdit_kullanici_parola );
+        MSG_ERROR(tr ( "Invalid password" ) , m_ui->lineEdit_kullanici_parola );
         return ADAK_FAIL;
     }
 
@@ -166,11 +166,11 @@ void KULLANICI_DEGISTIR_BATCH::RUN_BATCH()
     sql_query.SET_VALUE( ":veritabani_ismi", VERITABANI_ISMI() );
 
     if (sql_query.SELECT() EQ 0 AND m_kullanici_id NE 1) {
-        MSG_ERROR(tr ( "Seçili kullanıcının bu veritabanında çalışma izni yok.\nYeni veritabanı seçiniz." ) , NULL);
+        MSG_ERROR(tr ( "The user selected no authorized to work in this database.\nSelect a new database." ) , NULL);//Seçili kullanıcının bu veritabanında çalışma izni yok. Yeni veritabanı seçiniz.
         if ( VERITABANI_DEGISTIR(&DB,this) EQ -1 ) {
             exit(88);
         }
-        MSG_INFO(tr("Firma değişikliği başarılı. \nSeçilen firma: "+ VERITABANI_ISMI().toUtf8() + " " + VERITABANI_TANIMI().toUtf8() ),NULL);
+        MSG_INFO(tr("Company change successful. \nSelected Company: "+ VERITABANI_ISMI().toUtf8() + " " + VERITABANI_TANIMI().toUtf8() ),NULL);//Firma değişikliği başarılı.\n Seçilen Firma:
     }
 }
 
