9291b21df3387e445c5b05602ef802272782050d
Bahar TİMAÇ <bahar.timac@gmail.com>
2016-01-19 16:49:11 +0200

    ODS_IO_TR dosyası eklendi.

---
+++ ODS_IO/ODS_IO_tr.ts
@@ -0,0 +1,162 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="tr_TR">
+<context>
+    <name>EXPORT_KERNEL</name>
+    <message>
+        <location filename="export_kernel.cpp" line="26"/>
+        <source>Transfer to OpenOffice.</source>
+        <oldsource>OpenOffice&apos;e Aktar</oldsource>
+        <translation>OpenOffice&apos;e Aktar.</translation>
+    </message>
+    <message>
+        <location filename="export_kernel.cpp" line="28"/>
+        <source>EXPORT</source>
+        <translation>İHRAÇ ET</translation>
+    </message>
+    <message>
+        <location filename="export_kernel.cpp" line="74"/>
+        <source>Save</source>
+        <translation>Kaydet</translation>
+    </message>
+    <message>
+        <location filename="export_kernel.cpp" line="74"/>
+        <source>ODS File(*.ods)</source>
+        <translation>ODS Dosyası(*.ods)</translation>
+    </message>
+    <message>
+        <location filename="export_kernel.cpp" line="87"/>
+        <source>Transferred Information.</source>
+        <translation>Bilgi Transferi.</translation>
+    </message>
+</context>
+<context>
+    <name>IMPORT_KERNEL</name>
+    <message>
+        <location filename="import_kernel.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Diyalog</translation>
+    </message>
+    <message>
+        <location filename="import_kernel.ui" line="41"/>
+        <source>File Encoding :  </source>
+        <translation>Dosya Kodlama:</translation>
+    </message>
+    <message>
+        <location filename="import_kernel.ui" line="92"/>
+        <source>Those registered in the database ...</source>
+        <translation>Veri tabanında kayıtlı olanlar...</translation>
+    </message>
+    <message>
+        <location filename="import_kernel.ui" line="102"/>
+        <source>Groups of records to be added:</source>
+        <translation>Kayıtların Ekleneceği Gruplar:</translation>
+    </message>
+    <message>
+        <location filename="import_kernel.ui" line="119"/>
+        <source>Select Group</source>
+        <translation>Grup Seç</translation>
+    </message>
+    <message>
+        <location filename="import_kernel.ui" line="154"/>
+        <source>File:</source>
+        <translation>Dosya:</translation>
+    </message>
+    <message>
+        <location filename="import_kernel.ui" line="170"/>
+        <location filename="import_kernel.cpp" line="73"/>
+        <source>Select File</source>
+        <translation>Dosya Seç</translation>
+    </message>
+    <message>
+        <location filename="import_kernel.ui" line="201"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Ubuntu&apos;; font-size:11pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans Serif&apos;; font-size:10pt;&quot;&gt;Preview(&lt;/span&gt;&lt;a name=&quot;result_box&quot;&gt;&lt;/a&gt;&lt;span style=&quot; color:#ff0000;&quot;&gt;I&lt;/span&gt;&lt;span style=&quot; color:#ff0000;&quot;&gt;f&lt;/span&gt;&lt;span style=&quot; color:#ff0000;&quot;&gt; the location of the preview information is incorrect, the file will be information here that need to be corrected according to the order.&lt;/span&gt;&lt;span style=&quot; font-family:&apos;Sans Serif&apos;; font-size:10pt;&quot;&gt;)&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Ubuntu&apos;; font-size:11pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans Serif&apos;; font-size:10pt;&quot;&gt;Önizleme(&lt;/span&gt;&lt;a name=&quot;result_box&quot;&gt;&lt;/a&gt;&lt;span style=&quot; color:#ff0000;&quot;&gt;I&lt;/span&gt;&lt;span style=&quot; color:#ff0000;&quot;&gt;f&lt;/span&gt;&lt;span style=&quot; color:#ff0000;&quot;&gt;
+Eğer bilgilerin yerleri önizlemede yanlışsa,  bilgilerin alınacağı dosyadan buradaki sıraya göre düzeltilmesi gereklidir..&lt;/span&gt;&lt;span style=&quot; font-family:&apos;Sans Serif&apos;; font-size:10pt;&quot;&gt;)&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="import_kernel.cpp" line="62"/>
+        <source>UTF-8</source>
+        <translation>UTF-8</translation>
+    </message>
+    <message>
+        <location filename="import_kernel.cpp" line="80"/>
+        <source>The selected file format is not appropriate!</source>
+        <oldsource>Seçilen dosya Formatı uygun değil!!!</oldsource>
+        <translation>Seçilen dosya formatı uygun değil!</translation>
+    </message>
+    <message>
+        <location filename="import_kernel.cpp" line="91"/>
+        <source>The selected file is empty.</source>
+        <oldsource>Seçilen dosyanın içi boştur</oldsource>
+        <translation>Seçilen dosyanın içi boştur.</translation>
+    </message>
+    <message>
+        <location filename="import_kernel.cpp" line="35"/>
+        <source>Import</source>
+        <translation>içe aktar</translation>
+    </message>
+    <message>
+        <location filename="import_kernel.cpp" line="42"/>
+        <source>IMPORT</source>
+        <translation>İÇE AKTAR</translation>
+    </message>
+    <message>
+        <location filename="import_kernel.cpp" line="73"/>
+        <source>ODS File(*.ods)</source>
+        <translation>ODS Dosyası(*.ods)</translation>
+    </message>
+    <message>
+        <location filename="import_kernel.cpp" line="144"/>
+        <source>No data will be saved</source>
+        <translation>kaydedilecek veri yok</translation>
+    </message>
+    <message>
+        <location filename="import_kernel.cpp" line="148"/>
+        <source>Required fields are left empty </source>
+        <translation>Zorunlu alanlar boş bırakılmıştır</translation>
+    </message>
+    <message>
+        <location filename="import_kernel.cpp" line="152"/>
+        <source>All information will be transferred to E9.Continue? </source>
+        <translation>Tüm bilgiler E9&apos;a aktarılacaktır. Devam edilsin mi?</translation>
+    </message>
+    <message>
+        <location filename="import_kernel.cpp" line="177"/>
+        <source>All information transferred. </source>
+        <translation>Tüm bilgiler aktarıldı.</translation>
+    </message>
+    <message>
+        <location filename="import_kernel.cpp" line="189"/>
+        <source>Number of Transferred Records: %1</source>
+        <translation>Aktarılacak kayıt sayısı: %1</translation>
+    </message>
+    <message>
+        <location filename="import_kernel.cpp" line="272"/>
+        <location filename="import_kernel.cpp" line="280"/>
+        <source>@</source>
+        <translation>@</translation>
+    </message>
+    <message>
+        <location filename="import_kernel.cpp" line="274"/>
+        <source>In number %1 line, %2 Required fields were left blank</source>
+        <translation type="unfinished">%1 nolu satırda %2 zorunlu alanı boş bırakılmış</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="adak_exporter.cpp" line="280"/>
+        <source>Adak Import</source>
+        <translation>Adak içe aktar</translation>
+    </message>
+</context>
+</TS>
