2e6126d3ccfe4cc7b243c846e1ec7bb2f8cae8bb
Bahar TİMAÇ <bahar.timac@gmail.com>
2016-01-19 12:44:38 +0200

    DOT_Matrix ts dosyası upload edildi

---
+++ BELGE_KERNEL/DOT_MATRIX/dot_matrix_tr.ts
@@ -0,0 +1,48 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="tr_TR">
+<context>
+    <name>PRINTER_TEST</name>
+    <message>
+        <location filename="printer_test.ui" line="14"/>
+        <source>PRINTER_TEST</source>
+        <translation>YAZICI_TEST</translation>
+    </message>
+    <message>
+        <location filename="printer_test.ui" line="26"/>
+        <source>Send Printer</source>
+        <translation>Yazıcıya Gönder</translation>
+    </message>
+    <message>
+        <location filename="printer_test.ui" line="49"/>
+        <source>Quality</source>
+        <translation>Kalite</translation>
+    </message>
+    <message>
+        <location filename="printer_test.ui" line="55"/>
+        <location filename="printer_test.ui" line="84"/>
+        <source>Normal</source>
+        <translation>Normal</translation>
+    </message>
+    <message>
+        <location filename="printer_test.ui" line="62"/>
+        <source>NLQ</source>
+        <translation>NLQ</translation>
+    </message>
+    <message>
+        <location filename="printer_test.ui" line="78"/>
+        <source>Mode</source>
+        <translation>Mod</translation>
+    </message>
+    <message>
+        <location filename="printer_test.ui" line="91"/>
+        <source>Large</source>
+        <translation>Büyük</translation>
+    </message>
+    <message>
+        <location filename="printer_test.ui" line="98"/>
+        <source>Compressed</source>
+        <translation>Sıkıştırılmış</translation>
+    </message>
+</context>
+</TS>
