05f313cf3ced72d9d87ea33bf34ec6ae51661d2d
Bahar TİMAÇ <bahar.timac@gmail.com>
2016-01-19 14:18:42 +0200

    e-mail_tr.ts dosyası oluşturuldu

---
+++ EMAIL/email_tr.ts
@@ -0,0 +1,301 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="tr_TR">
+<context>
+    <name>EMAIL_SENDER</name>
+    <message>
+        <location filename="email_sender.cpp" line="22"/>
+        <source>Nameless</source>
+        <translation>İsimsiz</translation>
+    </message>
+    <message>
+        <location filename="email_sender.cpp" line="24"/>
+        <source>Not Described</source>
+        <translation>Tanımlı Değil</translation>
+    </message>
+    <message>
+        <location filename="email_sender.cpp" line="337"/>
+        <source>STARTTLS
+</source>
+        <oldsource>STARTTLS
+</oldsource>
+        <translation>START TLS
+</translation>
+    </message>
+    <message>
+        <location filename="email_sender.cpp" line="343"/>
+        <source>AUTH LOGIN
+</source>
+        <oldsource>AUTH LOGIN
+</oldsource>
+        <translation>AUTH GİRİŞ</translation>
+    </message>
+    <message>
+        <location filename="email_sender.cpp" line="360"/>
+        <source>MAIL FROM: &lt;</source>
+        <translation>POSTA KIMDEN: &lt;</translation>
+    </message>
+    <message>
+        <location filename="email_sender.cpp" line="366"/>
+        <source>RCPT TO: &lt;</source>
+        <translation>RCPT TO: &lt;</translation>
+    </message>
+    <message>
+        <location filename="email_sender.cpp" line="375"/>
+        <source>DATA
+</source>
+        <oldsource>DATA
+</oldsource>
+        <translation>VERİ
+</translation>
+    </message>
+    <message>
+        <location filename="email_sender.cpp" line="390"/>
+        <source>QUIT
+</source>
+        <oldsource>QUIT
+</oldsource>
+        <translation>ÇIKIŞ</translation>
+    </message>
+    <message>
+        <location filename="email_sender.cpp" line="524"/>
+        <source>Message-ID: &lt;%1@%2&gt;</source>
+        <translation>Mesaj ID: &lt;%1@%2&gt;</translation>
+    </message>
+    <message>
+        <location filename="email_sender.cpp" line="525"/>
+        <source>
+Date: %1</source>
+        <translation>Tarih:%1</translation>
+    </message>
+    <message>
+        <location filename="email_sender.cpp" line="527"/>
+        <source>
+User-Agent: Adak/1.0 AdakEmailSender/1.0</source>
+        <translation>Kullanıcı-Distribütör: Adak/1.0 AdakEmailSender/1.0</translation>
+    </message>
+    <message>
+        <location filename="email_sender.cpp" line="528"/>
+        <source>
+To: </source>
+        <translation>Kime:</translation>
+    </message>
+    <message>
+        <location filename="email_sender.cpp" line="529"/>
+        <source>
+Subject: </source>
+        <translation>Konu:</translation>
+    </message>
+    <message>
+        <location filename="email_sender.cpp" line="553"/>
+        <source>Could not connect to Mail Server. ( STARTTLS/NOSSL ) 
+Socket Error : %1 %2 </source>
+        <oldsource>Posta Sunucusuna Bağlanılamadı. ( STARTTLS/NOSSL ) 
+Socket Hatasi : %1 %2 </oldsource>
+        <translation>Posta sunucuile bağlantı kurulamadı. ( STARTTLS/NOSSL ) 
+Socket Hata : %1 %2</translation>
+    </message>
+    <message>
+        <location filename="email_sender.cpp" line="568"/>
+        <source>Could not connect to Mail Server. ( TLS1/SSL2/SSL3 ) 
+Socket Error : %1 %2 </source>
+        <oldsource>Posta Sunucusuna Bağlanılamadı. ( TLS1/SSL2/SSL3 ) 
+Socket Hatasi : %1 %2 </oldsource>
+        <translation>Posta Sunucusuna Bağlanılamadı. ( TLS1/SSL2/SSL3 ) \nSocket Hatasi : %1 %2</translation>
+    </message>
+    <message>
+        <location filename="email_sender.cpp" line="294"/>
+        <source>Sending Your Message...</source>
+        <oldsource>Mesajınız Gönderiliyor...</oldsource>
+        <translation>Mesajınız Gönderiliyor...</translation>
+    </message>
+</context>
+<context>
+    <name>EMAIL_SENDER_GUI</name>
+    <message>
+        <location filename="email_sender_gui.ui" line="14"/>
+        <source>EMAIL GUI</source>
+        <translation>EPOSTA GUI</translation>
+    </message>
+    <message>
+        <location filename="email_sender_gui.ui" line="23"/>
+        <source>Message Information</source>
+        <translation>Mesaj Bilgileri</translation>
+    </message>
+    <message>
+        <location filename="email_sender_gui.ui" line="29"/>
+        <source>From:</source>
+        <oldsource>From::</oldsource>
+        <translation>Kİmden:</translation>
+    </message>
+    <message>
+        <location filename="email_sender_gui.ui" line="50"/>
+        <source>To:</source>
+        <translation>Kime:</translation>
+    </message>
+    <message>
+        <location filename="email_sender_gui.ui" line="60"/>
+        <source>Subject:</source>
+        <translation>Konu:</translation>
+    </message>
+    <message>
+        <location filename="email_sender_gui.ui" line="143"/>
+        <source>Server Setting</source>
+        <translation>Sunucu Ayarları</translation>
+    </message>
+    <message>
+        <location filename="email_sender_gui.ui" line="149"/>
+        <source>Mail Server:</source>
+        <oldsource>Mail Server::</oldsource>
+        <translation>Posta Sunucusu:</translation>
+    </message>
+    <message>
+        <location filename="email_sender_gui.ui" line="163"/>
+        <source>Port:</source>
+        <translation>Port:</translation>
+    </message>
+    <message>
+        <location filename="email_sender_gui.ui" line="183"/>
+        <source>Connection Security:</source>
+        <translation>Bağlantı Güvenliği:</translation>
+    </message>
+    <message>
+        <location filename="email_sender_gui.ui" line="194"/>
+        <source>NOPE</source>
+        <translation>YOK</translation>
+    </message>
+    <message>
+        <location filename="email_sender_gui.ui" line="199"/>
+        <source>SSLV2</source>
+        <translation>SSLV2</translation>
+    </message>
+    <message>
+        <location filename="email_sender_gui.ui" line="204"/>
+        <source>SSLV3</source>
+        <translation>SSLV3</translation>
+    </message>
+    <message>
+        <location filename="email_sender_gui.ui" line="209"/>
+        <source>TLSV1</source>
+        <translation>TLSV1</translation>
+    </message>
+    <message>
+        <location filename="email_sender_gui.ui" line="214"/>
+        <source>STARTTLS</source>
+        <translation>STARTTLS</translation>
+    </message>
+    <message>
+        <location filename="email_sender_gui.ui" line="222"/>
+        <source>Use User Name and Password</source>
+        <translation>Kullanıcı Adı Ve Şifre Kullanılsın</translation>
+    </message>
+    <message>
+        <location filename="email_sender_gui.ui" line="241"/>
+        <source>E-Mail User Name:</source>
+        <translation>E-Posta Kullanıcı Adı:</translation>
+    </message>
+    <message>
+        <location filename="email_sender_gui.ui" line="255"/>
+        <source>E-Mail Password:</source>
+        <translation>E-Posta Şifre:</translation>
+    </message>
+    <message>
+        <location filename="email_sender_gui.ui" line="75"/>
+        <source>Message:</source>
+        <translation>Mesaj:</translation>
+    </message>
+    <message>
+        <location filename="email_sender_gui.ui" line="98"/>
+        <source>about:blank</source>
+        <translation>about:blank</translation>
+    </message>
+    <message>
+        <location filename="email_sender_gui.cpp" line="42"/>
+        <source>Send</source>
+        <translation>Gönder</translation>
+    </message>
+    <message>
+        <location filename="email_sender_gui.cpp" line="44"/>
+        <source>E-MAIL SENDING</source>
+        <translation>EPOSTA GÖNDERİLİYOR</translation>
+    </message>
+    <message>
+        <location filename="email_sender_gui.cpp" line="78"/>
+        <source>Mail server did not define.It needs to be defined from &apos;System-Mail Server&apos; menu.</source>
+        <oldsource>Posta Sunucusu Tanımlanmamış.&apos;Sistem-&gt;Posta Sunucusu&apos; menüsünden tanımlanması gerekli.</oldsource>
+        <translation>Posta Sunucusu Tanımlanmamış.&apos;Sistem-&gt;Posta Sunucusu&apos; menüsünden tanımlanması gerekli.</translation>
+    </message>
+    <message>
+        <location filename="email_sender_gui.cpp" line="93"/>
+        <source>Your E-Mail address or E-Mail User name did not define. It needs to be defined from &apos;System-Users&apos; menu.</source>
+        <translation>E-Posta adresiniz veya E-Posta Kullanıcı isminiz tanımlanmamış.&apos;Sistem-&gt;Kullanıcılar&apos; menüsünden tanımlanması gerekli.</translation>
+    </message>
+    <message>
+        <location filename="email_sender_gui.cpp" line="117"/>
+        <source>Sender e-mail address entered as invalid address.... </source>
+        <translation>Gönderen olarak girilen e-posta adresi geçersiz...</translation>
+    </message>
+    <message>
+        <location filename="email_sender_gui.cpp" line="125"/>
+        <source>Recipient e-mail address entered as invalid address.. </source>
+        <translation>Alıcı olarak girilen e-posta adresi geçersiz..</translation>
+    </message>
+    <message>
+        <location filename="email_sender_gui.cpp" line="178"/>
+        <source>Sended Your Message...</source>
+        <translation>Mesajınız Gönderildi...</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="email_sender.cpp" line="322"/>
+        <source>HELLO server
+</source>
+        <oldsource>HELLO server
+</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="email_sender.cpp" line="736"/>
+        <source>These e-mail address are wrong.</source>
+        <oldsource>Aşagıdaki e-mail adres(ler)i hatalıdır. </oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="email_sender.cpp" line="737"/>
+        <source>
+Incorrect E-Mail Addresses: </source>
+        <oldsource>
+Hatalı E-Mail Adresleri: </oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="email_sender.cpp" line="744"/>
+        <source>Do you want to send the message to current e-mail?</source>
+        <oldsource>Geçerli olan e-maillere mesajı göndermek ister misiniz?</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="email_sender.cpp" line="765"/>
+        <source>No e-mail address will be sent.</source>
+        <oldsource>Gönderilecek e-mail adresi bulunamadı.</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="email_sender.cpp" line="821"/>
+        <source>It below error(s) occured during sending e-mail.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="email_sender.cpp" line="835"/>
+        <source>There was an error at e-mails when sending e-mail.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="email_sender.cpp" line="858"/>
+        <source>There was an error can not be determined.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
